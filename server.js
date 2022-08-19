cbe35b77ef38578157ce7bc4bf7894fc16ab281c
clo0868 <60488913+clo0868@users.noreply.github.com>
2022-08-19 12:48:48 +1200

    and one

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.set("view engine", "ejs");
 app.use(express.json());
 app.use(cors());
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "pipeband.herokuapp.com");
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
