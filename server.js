346e190f7a9f22dcfe5d214090c01dbc8e7d2a25
clo0868 <60488913+clo0868@users.noreply.github.com>
2022-08-19 12:51:48 +1200

    one more

--- server.js
+++ server.js
@@ -29,18 +29,7 @@ server.listen(process.env.PORT || 3000)
 app.set("view engine", "ejs");
 app.use(express.json());
 app.use(cors());
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
-  );
-  next();
-});
+
 
 
 
