b7391a4914b99124b110b1b9ea0e57a70ed5c2d3
clo0868 <60488913+clo0868@users.noreply.github.com>
2022-08-19 13:07:26 +1200

    preflight

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ function hash(string) {
 console.log("i am listening");
 var server = http.createServer(app)
 server.listen(process.env.PORT || 3000)
-
+app.options('*', cors())
 app.set("view engine", "ejs");
 app.use(express.json());
 app.use(cors());
