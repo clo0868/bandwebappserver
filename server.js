a6f8920d92a9b9cc207366837f8bd5fce74652fb
clo0868 <60488913+clo0868@users.noreply.github.com>
2022-09-02 11:33:22 +1200

    clear parents before update

--- server.js
+++ server.js
@@ -132,11 +132,17 @@ app.post('/signup', function(req, res) {
 app.post('/update_children',auth, function(req, res) {
   const children = req.body.children 
   const user = req.user
-  var sql = 'UPDATE users SET parent = ? WHERE userID IN (?) ';
-  con.query(sql,[user.userID,children], function (err, r) {
+  var sql = 'UPDATE users SET parent = 0 WHERE parent = ? ';
+  con.query(sql,[user.userID], function (err, r) {
     if (err) throw err;
-    res.send(r);
+    var sql = 'UPDATE users SET parent = ? WHERE userID IN (?) ';
+    con.query(sql,[user.userID,children], function (err, r) {
+      if (err) throw err;
+      res.send(r);
+    });
+
   });
+  
 });
 app.post('/all_comp_data', auth, function(req, res) {
   var sql = 'SELECT * FROM competitions';
